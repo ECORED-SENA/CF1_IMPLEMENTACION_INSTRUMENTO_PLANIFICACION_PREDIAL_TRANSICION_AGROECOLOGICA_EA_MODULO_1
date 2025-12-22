6bd79eb7b6c012aabf4d0fd5968493dfb4d27625
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2025-12-22 08:29:40 -0500

    Vinculación video

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -315,8 +315,8 @@
 
     figure(data-aos="zoom-in").mb-5
       .video.mb-1
-        iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-      figcaption(data-aos="zoom-in") <strong>Video</strong>: TEXTO
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/4ZqxwIBilbg" title="Clasificación por características sensoriales" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+      figcaption(data-aos="zoom-in") <strong>Video</strong>: clasificación por características sensoriales.
 
     .row.align-items-center.mb-0
       .col-xl
